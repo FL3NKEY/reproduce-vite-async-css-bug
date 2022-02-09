3d115ee4845465e90f40f3a8788ad86888c25a81
FL3NKEY <noreply@example.com>
2022-02-09 14:31:25 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+- run `npm i`
+- run `npm run build`
+- run `npm run preview`
+
+Make sure that AsyncA and AsyncB have no applied css.
---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <root></root>
+    </div>
+</template>
+
+<script>
+import Root from 'reproduce-vite-async-css-bug-/components/Root.vue';
+
+export default {
+    components: {
+        Root
+    }
+}
+</script>
